80a7097916d232c67a1ea1fb38727a80150e9bca
Rishbh Sinha <sinharishabh@gmail.com>
2024-01-23 16:25:13 +0530

    initial

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        position ps=new position(0);
+        System.out.println("the initial position of player "+ ps.initial_position);
+
+    }
+}
---
+++ position.java
@@ -0,0 +1,9 @@
+public class position {
+    int initial_position;
+
+    position(int x) {
+        this.initial_position=x;
+    }
+
+
+}
