caecc50d3483b69c7a0effccd97bbc16c0ce9b3d
Rishbh Sinha <sinharishabh@gmail.com>
2024-01-23 16:52:14 +0530

    random dice

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        position ps=new position(0);
-        System.out.println("the initial position of player "+ ps.initial_position);
+
+import java.util.Random;
+        public class Main {
+            public static void main(String[] args) {
+                Random random=new Random();
+                position ps=new position(0);
+                System.out.println("the initial position of player "+ ps.initial_position);
+
+                int rolled_number = random.nextInt(1, 6);
+
+                System.out.println("The player rolled:"+rolled_number);
+
+
+
+
+
 
     }
 }
--- position.java
+++ position.java
@@ -1,8 +1,10 @@
 public class position {
     int initial_position;
 
+
     position(int x) {
         this.initial_position=x;
+
     }
 
 
