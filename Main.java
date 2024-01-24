517016b6fc2f814a81e81ef8c5d2011d3de8f79b
Rishbh Sinha <sinharishabh@gmail.com>
2024-01-24 15:05:57 +0530

    2 player

--- Main.java
+++ Main.java
@@ -2,52 +2,84 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Create an instance of the Random class
         Random random = new Random();
 
+        // Create instances of the Position class for two players with an initial position of 0
+        Position player1 = new Position(0);
+        Position player2 = new Position(0);
 
-        Position ps = new Position(0);
-        System.out.println("The initial position of player: " + ps.getInitialPosition());
-        int roll=0;
+        int rollsPlayer1 = 0; // Counter for the number of rolls for player 1
+        int rollsPlayer2 = 0; // Counter for the number of rolls for player 2
 
-        while (ps.getCurrentPosition() < 100) {
+        while (true) {
+            // Player 1's turn
+            int rolledNumberPlayer1 = random.nextInt(6) + 1;
+            rollsPlayer1++;
 
-            int rolledNumber = random.nextInt(6) + 1;
-            roll++;
-            System.out.println("no of times dice rolled: "+roll);
+            System.out.println("Player 1 - Roll #" + rollsPlayer1 + ": The player rolled: " + rolledNumberPlayer1);
 
-            System.out.println("The player rolled: " + rolledNumber);
-
-
-            int option = random.nextInt(3);
-            switch (option) {
+            int optionPlayer1 = random.nextInt(3); // Generates a random number between 0 and 2
+            switch (optionPlayer1) {
                 case 0:
-                    System.out.println("No Play. The player stays in the same position.");
+                    System.out.println("No Play. Player 1 stays in the same position.");
                     break;
                 case 1:
-                    System.out.println("Ladder. The player moves ahead by " + rolledNumber + " positions.");
-                    ps.moveAhead(rolledNumber);
+                    System.out.println("Ladder. Player 1 moves ahead by " + rolledNumberPlayer1 + " positions.");
+                    player1.moveAhead(rolledNumberPlayer1);
                     break;
                 case 2:
-                    System.out.println("Snake. The player moves behind by " + rolledNumber + " positions.");
-                    ps.moveBehind(rolledNumber);
+                    System.out.println("Snake. Player 1 moves behind by " + rolledNumberPlayer1 + " positions.");
+                    player1.moveBehind(rolledNumberPlayer1);
                     break;
             }
 
-
-            if (ps.getCurrentPosition() < 0) {
-                ps.setCurrentPosition(0);
+            // Ensure player 1's position doesn't go below 0
+            if (player1.getCurrentPosition() < 0) {
+                player1.setCurrentPosition(0);
             }
 
-             if (ps.getCurrentPosition() >100 ) {
-                ps.setCurrentPosition(100);
+            System.out.println("Player 1 - Current position: " + player1.getCurrentPosition());
+
+            // Check if Player 1 has won
+            if (player1.getCurrentPosition() >= 100) {
+                System.out.println("Player 1 won the game in " + rollsPlayer1 + " rolls.");
                 break;
             }
 
+            // Player 2's turn
+            int rolledNumberPlayer2 = random.nextInt(6) + 1;
+            rollsPlayer2++;
 
-            System.out.println("The current position of player: " + ps.getCurrentPosition());
+            System.out.println("Player 2 - Roll #" + rollsPlayer2 + ": The player rolled: " + rolledNumberPlayer2);
+
+            int optionPlayer2 = random.nextInt(3); // Generates a random number between 0 and 2
+            switch (optionPlayer2) {
+                case 0:
+                    System.out.println("No Play. Player 2 stays in the same position.");
+                    break;
+                case 1:
+                    System.out.println("Ladder. Player 2 moves ahead by " + rolledNumberPlayer2 + " positions.");
+                    player2.moveAhead(rolledNumberPlayer2);
+                    break;
+                case 2:
+                    System.out.println("Snake. Player 2 moves behind by " + rolledNumberPlayer2 + " positions.");
+                    player2.moveBehind(rolledNumberPlayer2);
+                    break;
+            }
+
+            // Ensure player 2's position doesn't go below 0
+            if (player2.getCurrentPosition() < 0) {
+                player2.setCurrentPosition(0);
+            }
+
+            System.out.println("Player 2 - Current position: " + player2.getCurrentPosition());
+
+            // Check if Player 2 has won
+            if (player2.getCurrentPosition() >= 100) {
+                System.out.println("Player 2 won the game in " + rollsPlayer2 + " rolls.");
+                break;
+            }
         }
-
-        System.out.println("Congratulations! The player reached the winning position: " + ps.getCurrentPosition());
     }
 }
